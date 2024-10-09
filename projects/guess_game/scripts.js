ed62fc45e0571db683e6c1784f7fabcece18521d
= <=>
2024-10-09 16:54:06 +0300

    final js of guess game

--- projects/guess_game/scripts.js
+++ projects/guess_game/scripts.js
@@ -1,13 +1,24 @@
 
-const secertNumber = Math.trunc(Math.random() * 20 + 1);
+let secertNumber = Math.trunc(Math.random() * 20 + 1);
 let score = 20;
+let highScore = 0;
 
 // show the number
 // document.querySelector(".number").textContent = secertNumber;
 
+// reset all to default value
 document.querySelector('.again').addEventListener('click', function() {
+
+    score = 20;
+    secertNumber = Math.trunc(Math.random() * 20 + 1);
+
     document.querySelector('body').style.backgroundColor = '#222';
     document.querySelector('.guess').value = ' ';
+    document.querySelector('.number').textContent = '?';
+    document.querySelector('.score').textContent = score;
+    document.querySelector(".number").style.width = '15rem';
+    document.querySelector('.message').textContent = 'Start guessing...';
+    document
 
 });
 
@@ -18,6 +29,7 @@ document.querySelector('.check').addEventListener('click', function(){
         // check if the guess input is empty
     if(!guess) {
         document.querySelector('.message').textContent = '⛔ No Number';
+        
         // check if guess is equal to secertNumber
     } else if(guess == secertNumber) {
         console.log("hello");
@@ -25,7 +37,14 @@ document.querySelector('.check').addEventListener('click', function(){
 
         // change the style page if correct
         document.querySelector('body').style.backgroundColor = '#60b347';
-        document.querySelector(".number").style.width = '30rem'
+        document.querySelector(".number").style.width = '30rem';
+        document.querySelector('.number').textContent = secertNumber;
+        
+        if(score > highScore) {
+            highScore = score;
+            document.querySelector('.highscore').textContent = highScore
+        }
+
 
         // if the guess greater than secertNumber print meassge
     } else if (guess > secertNumber) {
@@ -37,6 +56,7 @@ document.querySelector('.check').addEventListener('click', function(){
             document.querySelector('.message').textContent = '🔴 You Lost The Game'
             document.querySelector('.score').textContent = 0
         }
+        
         // if the guess less than secertNumber print meassge
     } else if (guess < secertNumber) {
         if (score > 1) {
