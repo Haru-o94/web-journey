b39f528a54a05cce0e777aff0cae2c709b7a77c8
= <=>
2024-10-09 11:22:26 +0300

    upload project guess game

---
+++ Learn JS/Projects/guess_game/scripts.js
@@ -0,0 +1,18 @@
+
+document.querySelector('.check').addEventListener('click', function(){
+
+    const guess = Number(document.querySelector('.guess').value);
+    console.log(guess)
+
+    if(!guess) {
+        document.querySelector('.message').textContent = '⛔ No Number';
+    }
+})
+
+
+
+
+
+
+
+
