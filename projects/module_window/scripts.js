2e81cdf9f47d5259393a3dad880820053a8ca3f4
= <=>
2024-10-10 11:47:16 +0300

    proect window modal done

---
+++ projects/module_window/scripts.js
@@ -0,0 +1,26 @@
+let modal = document.querySelector(".modal");
+let overlay = document.querySelector(".overlay");
+let btnCloseModal = document.querySelector(".close-modal");
+let btnOpenModal = document.querySelectorAll(".show-modal");
+
+const openModal = function () {
+  modal.classList.remove("hidden");
+  overlay.classList.remove("hidden");
+};
+
+const closeModal = function () {
+  modal.classList.add("hidden");
+  overlay.classList.add("hidden");
+};
+
+for (let i = 0; i < btnOpenModal.length; i++)
+  btnOpenModal[i].addEventListener("click", openModal);
+
+btnCloseModal.addEventListener("click", closeModal);
+overlay.addEventListener("click", closeModal);
+
+document.addEventListener("keydown", function (e) {
+  if (e.key === "Escape" && !modal.classList.contains("hidden")) {
+    closeModal();
+  }
+});
