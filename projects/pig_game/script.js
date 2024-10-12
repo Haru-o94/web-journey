82059505797ff092a539852c2d01555ca3e93899
= <=>
2024-10-12 22:22:25 +0300

    update script

--- projects/pig_game/script.js
+++ projects/pig_game/script.js
@@ -1,17 +1,20 @@
 "use strict";
 
+const player0Element = document.querySelector(".player--0");
+const player1Element = document.querySelector(".player--1");
 const score0Element = document.getElementById("score--0");
 const score1Element = document.getElementById("score--1");
 const diceElement = document.querySelector(".dice");
 const btnNew = document.querySelector(".btn--new");
 const btnRoll = document.querySelector(".btn--roll");
 const btnHold = document.querySelector(".btn--hold");
-const player0Score = document.querySelector("#current--0");
-const player1Score = document.querySelector("#current--1");
+const current0Element = document.querySelector("#current--0");
+const current1Element = document.querySelector("#current--1");
 
 // reset Element
 score0Element.textContent = 0;
 score1Element.textContent = 0;
+let activePlyer = 0;
 diceElement.classList.add("hidden");
 
 let currentScore = 0;
@@ -28,8 +31,13 @@ btnRoll.addEventListener("click", function () {
   // check if not 1 then continue play , else swtich player
   if (dice !== 1) {
     currentScore += dice;
-    player0Score.textContent = currentScore;
+    document.getElementById(`current--${activePlyer}`).textContent =
+      currentScore;
   } else {
-    player0Score.textContent = 0;
+    document.getElementById(`current--${activePlyer}`).textContent = 0;
+    currentScore = 0;
+    activePlyer = activePlyer === 0 ? 1 : 0;
+    player0Element.classList.toggle("player--active");
+    player1Element.classList.toggle("player--active");
   }
 });
